7af623f053107436de27c44b1544de788d9bfb87
David Harrold <david@dkxl.co.uk>
2025-03-26 12:12:15 +0000

    Restyle guest pages

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <x-favicon/>
     <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link href="/css/ocean.css" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <x-application-logo/>
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
-        </nav>
+<body id="@yield('body_id', 'guest')">
+<div id="wrapper" class="container-fluid">
 
-        <!-- Page Content -->
-        <main class="py-4">
-           {{ $slot }}
-        </main>
-        <x-footer/>
-    </div>
+    <div id="page-header" class="page-header hidden-print">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <x-application-logo/>
+            {{ config('app.name', 'Laravel') }}
+        </a>
+    </div><!-- page-header -->
+
+    <div id="content">
+        {{ $slot }}
+    </div> <!-- content -->
+
+    @yield('footer-nav')
+
+    <x-footer/>
+
+</div> <!-- wrapper -->
+
 </body>
 </html>
